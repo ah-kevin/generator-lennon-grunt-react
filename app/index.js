b76785e32bd150123b4af124a9637e06deb9c356
mac-Leoneo <kbjwdh@gmail.com>
2015-06-01 23:04:10 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,221 @@
+'use strict';
+var join = require('path').join;
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var wiredep = require('wiredep');
+
+module.exports = yeoman.generators.Base.extend({
+
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+
+
+
+    this.option('skip-install', {
+      desc: 'Skips the installation of dependencies',
+      type: Boolean
+    });
+
+    this.option('skip-install-message', {
+      desc: 'Skips the message after the installation of dependencies',
+      type: Boolean
+    });
+    this.pkg = require('../package.json');
+  },
+
+  askFor: function () {
+    var done = this.async();
+
+    // welcome message
+    if (!this.options['skip-welcome-message']) {
+      this.log(require('yosay')());
+      this.log(chalk.magenta(
+        'Out of the box I include HTML5 Boilerplate, jQuery, and a ' +
+        'Gruntfile.js to build your app.'
+      ));
+    }
+
+    var prompts = [{
+      type: 'checkbox',
+      name: 'features',
+      message: 'What more would you like?',
+      choices: [{
+        name: 'Bootstrap',
+        value: 'includeBootstrap',
+        checked: true
+      },{
+        name: 'Sass',
+        value: 'includeSass',
+        checked: false
+      },{
+        name: 'Modernizr',
+        value: 'includeModernizr',
+        checked: false
+      }]
+    }];
+
+    this.prompt(prompts, function (answers) {
+      var features = answers.features;
+
+      function hasFeature(feat) {
+        return features && features.indexOf(feat) !== -1;
+      }
+
+      this.includeSass = hasFeature('includeSass');
+      this.includeBootstrap = hasFeature('includeBootstrap');
+      this.includeModernizr = hasFeature('includeModernizr');
+
+      done();
+    }.bind(this));
+  },
+
+  gruntfile: function () {
+    this.template('Gruntfile.js');
+  },
+
+  packageJSON: function () {
+    this.template('_package.json', 'package.json');
+  },
+
+  git: function () {
+    this.template('gitignore', '.gitignore');
+    this.copy('gitattributes', '.gitattributes');
+  },
+
+  bower: function () {
+    var bower = {
+      name: this._.slugify(this.appname),
+      private: true,
+      dependencies: {}
+    };
+
+    if (this.includeBootstrap) {
+      var bs = 'bootstrap' + (this.includeSass ? '-sass-official' : '');
+      bower.dependencies[bs] = '~3.3.0';
+    } else {
+      bower.dependencies.jquery = '~1.11.1';
+    }
+
+    if (this.includeModernizr) {
+      bower.dependencies.modernizr = '~2.8.2';
+    }
+
+    this.copy('bowerrc', '.bowerrc');
+    this.write('bower.json', JSON.stringify(bower, null, 2));
+  },
+
+  jshint: function () {
+    this.copy('jshintrc', '.jshintrc');
+  },
+
+  editorConfig: function () {
+    this.copy('editorconfig', '.editorconfig');
+  },
+
+  mainStylesheet: function () {
+    var css = 'main.' + (this.includeSass ? 's' : '') + 'css';
+    this.template(css, 'app/styles/' + css);
+  },
+
+  writeIndex: function () {
+    this.indexFile = this.src.read('index.html');
+    this.indexFile = this.engine(this.indexFile, this);
+
+    // wire Bootstrap plugins
+    if (this.includeBootstrap && !this.includeSass) {
+      var bs = 'bower_components/bootstrap/js/';
+
+      this.indexFile = this.appendFiles({
+        html: this.indexFile,
+        fileType: 'js',
+        optimizedPath: 'scripts/plugins.js',
+        sourceFileList: [
+          bs + 'affix.js',
+          bs + 'alert.js',
+          bs + 'dropdown.js',
+          bs + 'tooltip.js',
+          bs + 'modal.js',
+          bs + 'transition.js',
+          bs + 'button.js',
+          bs + 'popover.js',
+          bs + 'carousel.js',
+          bs + 'scrollspy.js',
+          bs + 'collapse.js',
+          bs + 'tab.js'
+        ],
+        searchPath: '.'
+      });
+    }
+
+    this.indexFile = this.appendFiles({
+      html: this.indexFile,
+      fileType: 'js',
+      optimizedPath: 'scripts/main.js',
+      sourceFileList: ['scripts/main.js'],
+      searchPath: ['app', '.tmp']
+    });
+  },
+
+  app: function () {
+    this.directory('app');
+    this.mkdir('app/scripts');
+    this.mkdir('app/styles');
+    this.mkdir('app/images');
+    this.write('app/index.html', this.indexFile);
+
+    this.copy('main.js', 'app/components/main.js');
+
+  },
+
+  install: function () {
+
+    var howToInstall =
+        '\nAfter running ' +
+        chalk.yellow.bold('npm install & bower install') +
+        ', inject your' +
+        '\nfront end dependencies by running ' +
+        chalk.yellow.bold('grunt wiredep') +
+        '.';
+
+    if (this.options['skip-install']) {
+        this.log(howToInstall);
+         return;
+    }
+    this.installDependencies({
+      skipMessage: this.options['skip-install-message'],
+      skipInstall: this.options['skip-install']
+    });
+
+    this.on('end', function () {
+      var bowerJson = this.dest.readJSON('bower.json');
+
+      // wire Bower packages to .html
+      wiredep({
+        bowerJson: bowerJson,
+        directory: 'bower_components',
+        exclude: ['bootstrap-sass', 'bootstrap.js'],
+        ignorePath: /^(\.\.\/)*\.\./,
+        src: 'app/index.html'
+      });
+
+      if (this.includeSass) {
+        // wire Bower packages to .scss
+        wiredep({
+          bowerJson: bowerJson,
+          directory: 'bower_components',
+          ignorePath: /^(\.\.\/)+/,
+          src: 'app/styles/*.scss'
+        });
+      }
+      // ideally we should use composeWith, but we're invoking it here
+      // because generator-mocha is changing the working directory
+      // https://github.com/yeoman/generator-mocha/issues/28
+      //this.invoke(this.options['test-framework'], {
+      //  options: {
+      //    'skip-message': this.options['skip-install-message'],
+      //    'skip-install': this.options['skip-install']
+      //  }
+      //});
+    }.bind(this));
+  }
+});
